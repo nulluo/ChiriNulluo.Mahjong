804ea6cfbf0d075aca28075a30a75dd3d7559199
nulluo <nulluo.pg@gmail.com>
2018-10-20 13:25:56 +0900

    立直宣言牌を強調表示するための改修開始

--- ChiriNulluo.Mahjong.WinFormApplication/View/Controls/HorizontalDiscardedPilePanel.vb
+++ ChiriNulluo.Mahjong.WinFormApplication/View/Controls/HorizontalDiscardedPilePanel.vb
@@ -85,13 +85,17 @@ Namespace View
                 _baseImage = My.Resources.FallenTileBase
             End If
 
+            If True Then
+                _baseImage = CreateColorCorrectedImage(_baseImage, 1.0, 0.7, 0.7, 0, 0, 0)
+            End If
+
             Dim _g As Graphics = Graphics.FromImage(_baseImage)
 
             '白牌にキャラ絵・テキストを描画
             Dim _diffImage As Bitmap = DirectCast(tile, Precure.Tiles.PreCureCharacterTile).Image
 
             'Dim _diffImage As Image = My.Resources.ResourceManager.GetObject(String.Format("Precure{0:0000}", tileID))
-            _g.DrawImage(_diffImage, 0, 3)
+            _g.DrawImage(_diffImage, 0, 0, 48, 53)
 
             'リソース解放
             _diffImage.Dispose()
@@ -130,5 +134,47 @@ Namespace View
             Me.SetAllTileImages()
         End Sub
 
+
+        ''' <summary>
+        ''' 指定した画像の色を補正した画像を取得する
+        ''' </summary>
+        ''' <param name="img">色の補正をする画像</param>
+        ''' <param name="rScale">赤に掛ける倍率</param>
+        ''' <param name="gScale">緑に掛ける倍率</param>
+        ''' <param name="bScale">青に掛ける倍率</param>
+        ''' <param name="rAdd">赤に対する加算</param>
+        ''' <param name="gAdd">緑に対する加算</param>
+        ''' <param name="bAdd">青に対する加算</param>
+        ''' <returns></returns>
+        Private Shared Function CreateColorCorrectedImage(
+            ByVal img As Image, ByVal rScale As Single,
+            ByVal gScale As Single, ByVal bScale As Single,
+            ByVal rAdd As Single, gAdd As Single, bAdd As Single) As Image
+
+            '補正された画像の描画先となるImageオブジェクトを作成
+            Dim newImg As New Bitmap(img.Width, img.Height)
+            'newImgのGraphicsオブジェクトを取得
+            Using g = Graphics.FromImage(newImg)
+                'ColorMatrixオブジェクトの作成
+                '指定された倍率を掛けるための行列を指定する
+                Dim cm As New System.Drawing.Imaging.ColorMatrix(New Single()() _
+                {New Single() {rScale, 0, 0, 0, 0},
+                 New Single() {0, gScale, 0, 0, 0},
+                 New Single() {0, 0, bScale, 0, 0},
+                 New Single() {0, 0, 0, 1, 0},
+                 New Single() {rAdd, gAdd, bAdd, 0, 1}})
+
+                'ImageAttributesオブジェクトの作成
+                Dim ia As New System.Drawing.Imaging.ImageAttributes()
+                'ColorMatrixを設定する
+                ia.SetColorMatrix(cm)
+
+                'ImageAttributesを使用して描画
+                'g.DrawImage(img, New Rectangle(0, 0, img.Width, 53),
+                g.DrawImage(img, New Rectangle(0, 0, img.Width, img.Height),
+                    0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia)
+            End Using
+            Return newImg
+        End Function
     End Class
 End Namespace
