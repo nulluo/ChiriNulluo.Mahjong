6d2bd705c93ed1c2852d43dc8f00f4802eef8f46
nulluo <nulluo.pg@gmail.com>
2018-12-21 20:42:53 +0900

    実行環境のフォルダを整理するため、ゲームEXE本体を起動するEXEを追加
    ChiriNulluo.Mahjong.Starterの追追

---
+++ ChiriNulluo.Mahjong.Starter/Form1.vb
@@ -0,0 +1,8 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Shell(Path.Combine(My.Application.Info.DirectoryPath, "Game\CureJongMain.exe"), AppWinStyle.NormalFocus)
+        Application.Exit()
+    End Sub
+End Class
