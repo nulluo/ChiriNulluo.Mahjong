e4062c847a362d6b3c6a51a4612bfc39da048036
nulluo <nulluo.pg@gmail.com>
2018-12-21 19:22:32 +0900

    アプリケーションアイコンを視認しやすく修正

--- ChiriNulluo.Mahjong.WinFormApplication/My Project/Resources.Designer.vb
+++ ChiriNulluo.Mahjong.WinFormApplication/My Project/Resources.Designer.vb
@@ -370,6 +370,16 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  (アイコン) に類似した型 System.Drawing.Icon のローカライズされたリソースを検索します。
+        '''</summary>
+        Friend ReadOnly Property CureJong3() As System.Drawing.Icon
+            Get
+                Dim obj As Object = ResourceManager.GetObject("CureJong3", resourceCulture)
+                Return CType(obj,System.Drawing.Icon)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
         '''</summary>
         Friend ReadOnly Property Face_Eas01() As System.Drawing.Bitmap
